f07147f74687f4c347a8af83c42c06d7ccc45dcf
chanthyhit <chanthy.hit@live.com>
2023-01-30 22:13:24 -0500

    Add JNDI into local profile

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 //@Configuration
 //@EnableAutoConfiguration
 //@ComponentScan
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -6,58 +6,102 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.annotation.PostConstruct;
+import javax.naming.NamingException;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@Profile({"local","test"})
-@PropertySource("classpath:application-local.properties")
 public class DataSourceConfig {
 
-    @Value("${spring.jpa.database-platform}")
-    private String platform;
-    @Value("${spring.datasource.driverClassName}")
-    private String driverClassName;
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
+    @Profile("local")
+    @PropertySource("classpath:application-local.properties")
+    class Local {
+        @Value("${spring.datasource.jndi-name}")
+        private String jndiName;
+        @Value("${spring.jpa.database-platform}")
+        private String platform;
+        @Bean
+        public DataSource jndiDataSource() throws NamingException {
+            JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+            bean.setJndiName(jndiName);
+            bean.setProxyInterface(DataSource.class);
+            bean.setLookupOnStartup(false);
+            bean.afterPropertiesSet();
+            return (DataSource) bean.getObject();
+        }
+        @Bean
+        public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+            LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+            entityManagerFactory.setDataSource(jndiDataSource());
+            entityManagerFactory.setPersistenceUnitName("MySQL");
+            entityManagerFactory.setPackagesToScan(new String[]{"com.example.demo.pojo"});
+            entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+            entityManagerFactory.setJpaProperties(jpaProperties());
+            return entityManagerFactory;
+        }
+        @Bean
+        public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.put("hibernate.dialect", platform);
+            properties.put("hibernate.show_sql", true);
+            return properties;
+        }
     }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(dataSource());
-        entityManagerFactory.setPersistenceUnitName("MySQL");
-        entityManagerFactory.setPackagesToScan(new String[]{"com.example.demo.pojo"});
-        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.dialect", platform);
-        entityManagerFactory.setJpaProperties(jpaProperties);
-        return entityManagerFactory;
-    }
+    @Profile("dev")
+    @PropertySource("classpath:application-dev.properties")
+    class Dev {
+        @Value("${spring.jpa.database-platform}")
+        private String platform;
+        @Value("${spring.datasource.driverClassName}")
+        private String driverClassName;
+        @Value("${spring.datasource.url}")
+        private String url;
+        @Value("${spring.datasource.username}")
+        private String username;
+        @Value("${spring.datasource.password}")
+        private String password;
 
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
+        @Bean
+        public DataSource dataSource() {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName(driverClassName);
+            dataSource.setUrl(url);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+            return dataSource;
+        }
+        @Bean
+        public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+            LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+            entityManagerFactory.setDataSource(dataSource());
+            entityManagerFactory.setPersistenceUnitName("MySQL");
+            entityManagerFactory.setPackagesToScan(new String[]{"com.example.demo.pojo"});
+            entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+            entityManagerFactory.setJpaProperties(jpaProperties());
+            return entityManagerFactory;
+        }
+        @Bean
+        public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.put("hibernate.dialect", platform);
+            properties.put("hibernate.show_sql", true);
+            return properties;
+        }
     }
 
 /*
