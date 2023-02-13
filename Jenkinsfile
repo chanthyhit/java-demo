04801645de463f123ef157adcf3e41a46f3b468d
chanthyhit <chanthy.hit@live.com>
2023-02-13 18:04:29 -0500

    Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -20,7 +20,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
+                sh './mvnw clean install -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.FORCE_BINARY_WRAPPER=true'
             }
         }
         stage('Test') {
--- src/test/java/com/example/demo/service/DepartmentServiceTests.java
+++ src/test/java/com/example/demo/service/DepartmentServiceTests.java
@@ -2,7 +2,8 @@ package com.example.demo.service;
 
 import com.example.demo.pojo.Department;
 import com.example.demo.repository.DepartmentRepository;
-import org.junit.jupiter.api.Test;
+
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,6 +11,10 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class DepartmentServiceTests {
@@ -20,7 +25,6 @@ public class DepartmentServiceTests {
     @Test
     public void findOneByIdTest(){
         int id = 1;
-        //Department expected = new Department(1,"401E","AAC01","DL","1");
         Department expected = new Department(1,"401E","AAC01","DL","1");
         when(repository.findOneById(id)).thenReturn(expected);
         Department actual = service.findOneById(id);
