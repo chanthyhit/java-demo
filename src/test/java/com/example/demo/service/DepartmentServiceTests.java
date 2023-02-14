ce75b8f54910f91aa07ced0696321c31469c8c20
chanthyhit <chanthy.hit@live.com>
2023-02-14 11:18:36 -0500

    Jenkinsfile

--- src/test/java/com/example/demo/service/DepartmentServiceTests.java
+++ src/test/java/com/example/demo/service/DepartmentServiceTests.java
@@ -3,29 +3,25 @@ package com.example.demo.service;
 import com.example.demo.pojo.Department;
 import com.example.demo.repository.DepartmentRepository;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.test.context.junit4.SpringRunner;
-
-//@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@ActiveProfiles("dev")
 public class DepartmentServiceTests {
     @Autowired
     DepartmentService service;
-    @MockBean
+    @Mock
     DepartmentRepository repository;
     @Test
     public void findOneByIdTest(){
         int id = 1;
-        Department expected = new Department(1,"401E","AAC01","DL","1");
+        Department expected = new Department(1,"401A","AAC01","DL","1");
         when(repository.findOneById(id)).thenReturn(expected);
         Department actual = service.findOneById(id);
         assertEquals(expected, actual);
